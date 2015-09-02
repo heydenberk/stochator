49fd2a610165878603a83db62201bf2bcbeaf63a
Eric Heydenberk <heydenberk@gmail.com>
2015-09-01 21:59:22 -0400

    Replace CoffeeScript thin arrows with ES6 fat arrows.

--- packages/stochator/index.js
+++ packages/stochator/index.js
@@ -1,29 +1,29 @@
 seedrandom = require("seedrandom")
 
-isType = (type) ->
-    (arg) -> Object::toString.call(arg) == "[object #{ type }]"
+isType = (type) =>
+    (arg) => Object::toString.call(arg) == "[object #{ type }]"
 
 isFunc = isType("Function")
 
 isObject = isType("Object")
 
-randomBoundedFloat = (prng, min = 0, max = 1) ->
+randomBoundedFloat = (prng, min = 0, max = 1) =>
     spread = max - min
     prng() * spread + min
 
-randomBoundedInteger = (prng, min = 0, max = 1) ->
+randomBoundedInteger = (prng, min = 0, max = 1) =>
     spread = 1 + max - min
     Math.floor(prng() * spread) + min
 
-randomColor = (prng) ->
+randomColor = (prng) =>
     byte = kind: "integer", min: 0, max: 255, prng: prng
-    mutator = (bytes) ->
+    mutator = (bytes) =>
         [red, green, blue] = bytes
         { red, green, blue }
 
     new Stochator(byte, byte, byte, mutator).next
 
-randomNormallyDistributedFloat = (prng, mean, stdev, min, max) ->
+randomNormallyDistributedFloat = (prng, mean, stdev, min, max) =>
     seed = randomBoundedFloat(prng)
     float = inverseNormalCumulativeDistribution(seed) * stdev + mean
     if min? and max?
@@ -31,23 +31,23 @@ randomNormallyDistributedFloat = (prng, mean, stdev, min, max) ->
     else
         float
 
-randomCharacter = (prng, lowercase) ->
+randomCharacter = (prng, lowercase) =>
     [min, max] = if lowercase then [97, 122] else [65, 90]
-    mutator = (charCode) -> String.fromCharCode(charCode)
+    mutator = (charCode) => String.fromCharCode(charCode)
     new Stochator({ kind: "integer", min, max, prng }, mutator).next
 
-randomSetMember = (prng, set) ->
+randomSetMember = (prng, set) =>
     max = set.length - 1
     set.get(randomBoundedInteger(prng, 0, max))
 
-randomSetMemberWithoutReplacement = (prng, set) ->
+randomSetMemberWithoutReplacement = (prng, set) =>
     return undefined unless set.get(0)
     set.length -= 1
     set.pop(randomBoundedInteger(prng, 0, set.length))
 
-randomWeightedSetMember = (prng, set, weights) ->
+randomWeightedSetMember = (prng, set, weights) =>
     [member, weightSum, float] = [undefined, 0, randomBoundedFloat(prng)]
-    set.each((value, index) ->
+    set.each((value, index) =>
         return if member
         weight = weights.get(index)
         if float <= weightSum + weight and float >= weightSum
@@ -57,7 +57,7 @@ randomWeightedSetMember = (prng, set, weights) ->
 
     member
 
-inverseNormalCumulativeDistribution = (probability) ->
+inverseNormalCumulativeDistribution = (probability) =>
     high = probability > 0.97575
     low = probability < 0.02425
 
@@ -90,15 +90,15 @@ inverseNormalCumulativeDistribution = (probability) ->
         coefficient = probability - 0.5
         base = Math.pow(coefficient, 2)
 
-    mapMaxExp = (maxExp) ->
-        (value, index) -> value * Math.pow(base, maxExp - index)
+    mapMaxExp = (maxExp) =>
+        (value, index) => value * Math.pow(base, maxExp - index)
 
     numerator = numCoefficients.map(mapMaxExp(numMaxExponent)).sum()
     denominator = denomCoeffcients.map(mapMaxExp(denomMaxExponent)).sum() + 1
 
     coefficient * numerator / denominator
 
-shuffleSet = (prng, set) ->
+shuffleSet = (prng, set) =>
     values = set.copy()
     for index in [values.length - 1...0]
         randomIndex = randomBoundedInteger(prng, 0, index)
@@ -109,32 +109,32 @@ shuffleSet = (prng, set) ->
 
     values
 
-floatGenerator = (prng, min, max, mean, stdev) ->
+floatGenerator = (prng, min, max, mean, stdev) =>
     if mean and stdev
-        -> randomNormallyDistributedFloat(prng, mean, stdev, min, max)
+        => randomNormallyDistributedFloat(prng, mean, stdev, min, max)
     else
-        -> randomBoundedFloat(prng, min, max)
+        => randomBoundedFloat(prng, min, max)
 
-integerGenerator = (prng, min = 0, max = 1) ->
-    -> randomBoundedInteger(prng, min, max)
+integerGenerator = (prng, min = 0, max = 1) =>
+    => randomBoundedInteger(prng, min, max)
 
-setGenerator = (prng, values, replacement = true, shuffle = false, weights = null) ->
+setGenerator = (prng, values, replacement = true, shuffle = false, weights = null) =>
     if not values or not values.length
         throw Error("Must provide a 'values' array for a set generator.")
 
     set = new Set(values)
     if shuffle
-        -> shuffleSet(prng, set)
+        => shuffleSet(prng, set)
     else if replacement
         if weights
             weightsSet = new Set(weights)
-            -> randomWeightedSetMember(prng, set, weightsSet)
+            => randomWeightedSetMember(prng, set, weightsSet)
         else
-            -> randomSetMember(prng, set)
+            => randomSetMember(prng, set)
     else
-        -> randomSetMemberWithoutReplacement(prng, set)
+        => randomSetMemberWithoutReplacement(prng, set)
 
-createGenerator = (config) ->
+createGenerator = (config) =>
     kind = config.kind or "float"
 
     defaultPrng = if config.seed then seedrandom else Math.random
@@ -157,20 +157,20 @@ createGenerator = (config) ->
     else
         generator
 
-getNextValueGenerator = (configs) ->
+getNextValueGenerator = (configs) =>
     configs[0] ?= {}
     generators = (createGenerator(config) for config in configs)
     if generators.length is 1
-        -> generators[0]()
+        => generators[0]()
     else
-        -> (generator() for generator in generators)
+        => (generator() for generator in generators)
 
 
 class Stochator
 
     VERSION = "0.4"
 
-    constructor: (configs..., mutator=null, name="next") ->
+    constructor: (configs..., mutator=null, name="next") =>
         # If the last arg is an object, all args are config args.
         # If the penultimate arg is an object, check whether the last arg
         # is a string (hence, the name) or a function (hence, the mutator).
@@ -186,24 +186,24 @@ class Stochator
 
         # If the mutator is provided, override the default identity func.
         if mutator
-            @mutate = (nextValue) -> mutator(nextValue, @getValue())
+            @mutate = (nextValue) => mutator(nextValue, @getValue())
 
         # Transform the configs to a func to get the next value.
         getNext = getNextValueGenerator(configs)
 
         # Assign `name` to the next mutated value(s), after `times` iterations.
         # If `times` is 1, just return the value, otherwise return an array.
-        @[name] = (times=1) ->
+        @[name] = (times=1) =>
             values = (@setValue(@mutate(getNext())) for time in [1..times])
             if times == 1 then values[0] else values
 
-    getValue: (value) -> @_value
+    getValue: (value) => @_value
 
-    mutate: (value) -> value
+    mutate: (value) => value
 
-    setValue: (value) -> @_value = value
+    setValue: (value) => @_value = value
 
-    toString: -> "[object Stochator]"
+    toString: => "[object Stochator]"
 
     _value: 0
 

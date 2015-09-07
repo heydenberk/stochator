cdea6f1ed3331e6607c4ab9a4b4f0138bf122873
Eric Heydenberk <heydenberk@gmail.com>
2015-09-07 16:06:11 -0400

    Add tests for all static methods.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,11 @@
+var _ = require('lodash');
 var assert = require('assert');
-var Stochator = require('stochator');
+var seedrandom = require('seedrandom');
+var Stochator = require('../build');
+
+var getPrng = function() {
+    return seedrandom('STOCHATOR');
+};
 
 describe('Stochator (with seed STOCHATOR)', function() {
 
@@ -12,53 +18,72 @@ describe('Stochator (with seed STOCHATOR)', function() {
     describe('the floating-point number generator', function() {
 
         it('should return a float between 0 and 1', function() {
-            var result = new Stochator({
+            var result1 = new Stochator({
                 seed: 'STOCHATOR'
             }).next();
-            assert.equal(result, 0.4045178783365678);
+            var result2 = Stochator.randomFloat(0, 1, getPrng());
+
+            assert.equal(result1, 0.4045178783365678);
+            assert.equal(result2, 0.4045178783365678);
         });
 
         it('should return a float between given bounds', function() {
-            var result = new Stochator({
+            var result1 = new Stochator({
                 min: 0,
                 max: Math.PI * 2,
                 seed: 'STOCHATOR'
             }).next();
-            assert.equal(result, 2.5416607896557823);
+            var result2 = Stochator.randomFloat(0, Math.PI * 2, getPrng());
+
+            assert.equal(result1, 2.5416607896557823);
+            assert.equal(result2, 2.5416607896557823);
         });
 
         it('should return a number from a normal distribution', function() {
-            var result = new Stochator({
+            var result1 = new Stochator({
                 mean: 75,
                 stdev: 14,
                 min: 0,
                 max: 100,
                 seed: 'STOCHATOR'
             }).next();
-            assert.equal(result, 71.61661782743502);
+            var result2 = Stochator.fromDistribution.normal(75, 14, 0, 100, getPrng());
+
+            assert.equal(result1, 71.61661782743502);
+            assert.equal(result2, 71.61661782743502);
         });
     });
 
     describe('the integer generator', function() {
 
         it('should return an integer between given bounds when "roll" is called', function() {
-            var result = new Stochator({
+            var result1 = new Stochator({
                 kind: "integer",
                 min: 1,
                 max: 6,
                 seed: "STOCHATOR"
             }, "roll").roll();
-            assert.equal(result, 3);
+            var result2 = Stochator.randomInteger(1, 6, getPrng());
+
+            assert.equal(result1, 3);
+            assert.equal(result2, 3);
         });
 
         it('should return an array of six integers between given bounds when "roll" is called', function() {
-            var result = new Stochator({
+            var result1 = new Stochator({
                 kind: "integer",
                 min: 1,
                 max: 6,
                 seed: "STOCHATOR"
             }, "roll").roll(5);
-            assert.deepEqual(result, [3, 6, 5, 1, 4]);
+
+            var prng = getPrng();
+            var result2 = _.range(5).map(function() {
+                return Stochator.randomInteger(1, 6, prng);
+            });
+
+            assert.deepEqual(result1, [3, 6, 5, 1, 4]);
+            assert.deepEqual(result2, [3, 6, 5, 1, 4]);
         });
 
     });
@@ -66,34 +91,56 @@ describe('Stochator (with seed STOCHATOR)', function() {
     describe('the set generator', function() {
 
         it('should return an item from a given array', function() {
-            var result = new Stochator({
+            var result1 = new Stochator({
                 kind: "set",
                 values: ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
                 seed: "STOCHATOR"
             }).next();
-            assert.equal(result, 'wednesday');
+            var result2 = Stochator.randomSetMember(
+                ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
+                getPrng());
+
+            assert.equal(result1, 'wednesday');
+            assert.equal(result2, 'wednesday');
         });
 
         it("should return 7 items from an array with replacement according to given weights", function() {
-            var result = new Stochator({
+            var result1 = new Stochator({
                 kind: "set",
                 values: ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
                 weights: [0.1, 0.1, 0.1, 0.1, 0.1, 0.25, 0.25],
                 seed: 'STOCHATOR'
             }).next(7);
-            assert.deepEqual(result, ['friday', 'sunday', 'sunday', 'monday', 'saturday', 'monday', 'saturday']);
+
+            var prng = getPrng();
+            var result2 = _.range(7).map(function() {
+                return Stochator.weightedRandomSetMember(
+                    ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
+                    [0.1, 0.1, 0.1, 0.1, 0.1, 0.25, 0.25],
+                    prng
+                );
+            });
+
+            assert.deepEqual(result1, ['friday', 'sunday', 'sunday', 'monday', 'saturday', 'monday', 'saturday']);
+            assert.deepEqual(result2, ['friday', 'sunday', 'sunday', 'monday', 'saturday', 'monday', 'saturday']);
         });
 
         it("should return 4 items from a set of 3 without replacement with a final item of undefined", function() {
-
-            var result = new Stochator({
+            var result1 = new Stochator({
                 kind: "set",
                 values: ["floors", "windows", "dishes"],
                 replacement: false,
                 seed: 'STOCHATOR'
             }).next(4);
-            assert.deepEqual(result, ['windows', 'dishes', 'floors', undefined]);
 
+            var prng = getPrng();
+            var set = ["floors", "windows", "dishes"];
+            var result2 = _.range(4).map(function() {
+                return Stochator.randomSetMemberWithoutReplacement(set, prng);
+            });
+
+            assert.deepEqual(result1, ['windows', 'dishes', 'floors', undefined]);
+            assert.deepEqual(result2, ['windows', 'dishes', 'floors', undefined]);
         });
 
     });
@@ -101,11 +148,14 @@ describe('Stochator (with seed STOCHATOR)', function() {
     describe('the color generator', function() {
 
         it("should return an object with red, green and blue values", function() {
-            var result = new Stochator({
+            var result1 = new Stochator({
                 kind: "color",
                 seed: 'STOCHATOR'
             }).next();
-            assert.deepEqual(result, { red: 103, green: 244, blue: 192 });
+            var result2 = Stochator.randomColor(getPrng());
+
+            assert.deepEqual(result1, { red: 103, green: 244, blue: 192 });
+            assert.deepEqual(result2, { red: 103, green: 244, blue: 192 });
         });
 
     });
@@ -113,11 +163,18 @@ describe('Stochator (with seed STOCHATOR)', function() {
     describe('the lowercase string generator', function() {
 
         it("should return 25 lowercase characters", function() {
-            var result = new Stochator({
+            var result1 = new Stochator({
                 kind: "a-z",
                 seed: 'STOCHATOR'
             }).next(25).join('');
-            assert.equal(result, 'kytapcrgjzwlmbbgxaeqlyspr');
+
+            var prng = getPrng();
+            var result2 = _.range(25).map(function() {
+                return Stochator.randomLowercaseCharacter(prng);
+            }).join('');
+
+            assert.equal(result1, 'kytapcrgjzwlmbbgxaeqlyspr');
+            assert.equal(result2, 'kytapcrgjzwlmbbgxaeqlyspr');
         });
 
     });
@@ -125,13 +182,16 @@ describe('Stochator (with seed STOCHATOR)', function() {
     describe('the integer generator with a mutator', function() {
 
         it("should return true or false", function() {
-            var result = new Stochator({
+            var result1 = new Stochator({
                 kind: "integer",
                 min: 0,
                 max: 1,
                 seed: 'STOCHATOR'
             }, Boolean).next();
-            assert.equal(result, false);
+            var result2 = Boolean(Stochator.randomInteger(0, 1, getPrng()));
+
+            assert.equal(result1, false);
+            assert.equal(result2, false);
         });
 
     });
@@ -139,12 +199,15 @@ describe('Stochator (with seed STOCHATOR)', function() {
     describe('the float generator with mutator', function() {
 
         it("should return the cosine of a given within given bounds", function() {
-            var result = new Stochator({
+            var result1 = new Stochator({
                 min: 0,
                 max: Math.PI * 2,
                 seed: 'STOCHATOR'
             }, Math.cos).next();
-            assert.equal(result, -0.8253740855106559);
+            var result2 = Math.cos(Stochator.randomFloat(0, Math.PI * 2, getPrng()));
+
+            assert.equal(result1, -0.8253740855106559);
+            assert.equal(result2, -0.8253740855106559);
         });
 
     });
@@ -152,27 +215,56 @@ describe('Stochator (with seed STOCHATOR)', function() {
     describe('the integer generator with mutator that uses previous value', function() {
 
         it("should return 10 integers that are all equal the previous item +/- 1", function() {
-            var result = new Stochator({
+            var result1 = new Stochator({
                 kind: "integer",
                 min: -1,
                 max: 1,
                 seed: 'STOCHATOR'
             }, function(a, b) { return a + b; }).next(10);
-            assert.deepEqual(result, [0, 1, 2, 1, 1, 0, 0, -1, -1, 0]);
+
+            var prng = getPrng();
+            var sum = 0;
+            var result2 = _.range(10).map(function() {
+                sum += Stochator.randomInteger(-1, 1, prng);
+                return sum;
+            });
+
+            assert.deepEqual(result1, [0, 1, 2, 1, 1, 0, 0, -1, -1, 0]);
+            assert.deepEqual(result2, [0, 1, 2, 1, 1, 0, 0, -1, -1, 0]);
         });
 
         it("should return 10 numbers that increase by a given fixed amount and a bounded relative change", function() {
             var addInterest = function(interestRate, principal) {
                 return (principal + 1000) * interestRate;
             };
-            var result = new Stochator({
+            var result1 = new Stochator({
                 kind: "float",
                 min: 1.01,
                 max: 1.05,
-                seed: 'STOCHATOR',
-                value: 1000
+                seed: 'STOCHATOR'
             }, addInterest).next(10);
-            assert.deepEqual(result, [
+
+            var prng = getPrng();
+            var principal = 0;
+            var result2 = _.range(10).map(function() {
+                var interestRate = Stochator.randomFloat(1.01, 1.05, prng);
+                principal = addInterest(interestRate, principal);
+                return principal;
+            });
+
+            assert.deepEqual(result1, [
+                1026.1807151334629,
+                2123.8934948333995,
+                3248.9834498702794,
+                4295.874912610078,
+                5475.947903284715,
+                6562.853629181616,
+                7838.825069340333,
+                9010.692918061377,
+                10256.214835559122,
+                11812.764583986824
+            ]);
+            assert.deepEqual(result2, [
                 1026.1807151334629,
                 2123.8934948333995,
                 3248.9834498702794,
@@ -194,8 +286,15 @@ describe('Stochator (with seed STOCHATOR)', function() {
         it('should return two integers between the given bounds', function() {
             var x = { kind: 'integer', min: 0, max: 480, seed: 'STOCHATOR' };
             var y = { kind: 'integer', min: 0, max: 360, seed: 'STOCHATOR' };
-            var result = new Stochator(x, y).next();
-            assert.deepEqual(result, [194, 146]);
+            var result1 = new Stochator(x, y).next();
+
+            var result2 = [
+                Stochator.randomInteger(0, 480, getPrng()),
+                Stochator.randomInteger(0, 360, getPrng())
+            ];
+
+            assert.deepEqual(result1, [194, 146]);
+            assert.deepEqual(result2, [194, 146]);
         });
 
     });
@@ -212,8 +311,15 @@ describe('Stochator (with seed STOCHATOR)', function() {
                     y: values[1]
                 };
             };
-            var result = new Stochator(x, y, mutator).next();
-            assert.deepEqual(result, {x: 194, y: 146});
+            var result1 = new Stochator(x, y, mutator).next();
+
+            var result2 = mutator([
+                Stochator.randomInteger(0, 480, getPrng()),
+                Stochator.randomInteger(0, 360, getPrng())
+            ]);
+
+            assert.deepEqual(result1, {x: 194, y: 146});
+            assert.deepEqual(result2, {x: 194, y: 146});
         });
 
     });
